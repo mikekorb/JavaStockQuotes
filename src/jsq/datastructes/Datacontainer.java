f759152f395e7c3a979f887148f1cf3c12df958c
Mike Korb <mike.korb@secure-mail.biz>
2015-01-04 21:32:48 +0100

    Datastructes changed

--- src/jsq/datastructes/Datacontainer.java
+++ src/jsq/datastructes/Datacontainer.java
@@ -1,10 +1,19 @@
 package jsq.datastructes;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Datacontainer {
 
-	public HashMap<String, Object> data = new HashMap<String, Object>();
+	public Map<String, Object> data;
+	
+	public Datacontainer() {
+		data = new HashMap<String, Object>();
+	}
+	
+	public Datacontainer(Map<String, Object> data) {
+		this.data = data;
+	}
 	
 	public void put(String key, Object value) {
 		data.put(key, value);
@@ -12,5 +21,9 @@ public class Datacontainer {
 	
 	public String toString() {
 		return data.entrySet().toString();
+	}
+	
+	public Map<String, Object> getMap() {
+		return data;
 	}
 }
